8b233e7d1c484e32a9c61dec0c917a94f19c41dd
Kona Keawekane <konakeawekane@gmail.com>
2026-02-25 17:10:43 -0700

    character card assignment

---
+++ charactercard/script.js
@@ -0,0 +1,46 @@
+let name = document.getElementById("name");
+let className = document.getElementById("class");
+let level = document.getElementById("level");
+let health = document.getElementById("health");
+let image = document.getElementById("profile");
+
+let character = {
+    name:"Cookie Monster",
+    class:"Destroyer of Cookies",
+    level:1,
+    health:100,
+    imgPath:"images/cookieMonster-og.png",
+    imgAlt:"cookie monster",
+    Attack: function(damage = 0){
+        let newHealth = this.health - damage;
+        this.health = Math.round(newHealth);
+        if(newHealth < 0){
+            this.health = 0;
+            alert("Character has died.");
+        }
+        this.RefreshData();
+    },
+    LevelUp: function(levels = 0){
+        this.level += Math.round(levels);
+        this.RefreshData();
+    },
+    RefreshData: function(){
+        name.textContent = this.name;
+        className.textContent = this.class;
+        level.textContent = this.level;
+        health.textContent = this.health;
+    }
+}
+
+let attackButton = document.getElementById("attack");
+let leveUpButton = document.getElementById("levelup");
+
+attackButton.addEventListener("click", e =>{
+    character.Attack(20);
+})
+
+leveUpButton.addEventListener("click", e =>{
+    character.LevelUp(1);
+})
+
+character.RefreshData();
