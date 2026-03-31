682511caa4f5c06dbda5f5657f70df0aba18bfb9
Kona Keawekane <konakeawekane@gmail.com>
2026-03-31 09:19:09 -0600

    final project basic html layout and js file system.

--- proceduralcanvas/drawer.js
+++ proceduralcanvas/drawer.js
@@ -1,6 +1,10 @@
-/** @type {HTMLCanvasElement} */
+/** @type {HTMLCanvasElement} */ //Used this to fix the Javascript checker to include canvas methods
+
+// page variables
 const canvas = document.getElementById("displayCanvas");
 const draw = canvas.getContext("2d");
+let redrawMode = "on change";
+let pageDarkMode = false;
 
 let instructions = [
     {
@@ -39,16 +43,112 @@ let instructions = [
     }
 ]
 
+// Replace this with the built in canvas pen drawing system
 let pen = {
     x: 0,
     y: 0,
     dir: 0
 }
 
-// setInterval(drawImage, 33);
+// setInterval(drawImage, 33); (30 fps)
+// clearInterval(drawImage, 33);
 
 drawImage();
 
+// Interation systems (adding instructions, removing insructions, changing theme, changing rendering mode, saving project, loading project)
+
+const redrawModeBtn = document.getElementById("redraw-mode-button");
+const darkModeBtn = document.getElementById("night-mode-button");
+const saveBtn = document.getElementById("save-button");
+const loadBtn = document.getElementById("load-button");
+const fileUploadInput = document.getElementById("json-upload");
+const reader = new FileReader();  // file reader for loading imported projects
+
+redrawModeBtn.addEventListener("click", changeRedrawMode);
+darkModeBtn.addEventListener("click", toggleTheme);
+saveBtn.addEventListener("click", saveProject);
+loadBtn.addEventListener("click", promptUploadProject);
+fileUploadInput.addEventListener("change", loadProject);
+reader.addEventListener("load", onLoad);
+
+
+function promptUploadProject(){
+    fileUploadInput.click();
+}
+
+// stubbed out
+function changeRedrawMode(){
+    switch(redrawMode){
+        case "on change":
+            break;
+        case "3 times a second":
+            break;
+        case "30 times a second":
+            break;
+        case "60 times a second":
+            break;
+        case "off":
+            break;
+        default:  // this should never be true
+            console.log("Invalid Redraw Mode Detected");
+            redrawMode = "on change";
+            break;
+    }
+}
+
+// stubbed out
+function toggleTheme(){
+    if(pageDarkMode){
+        // set to light mode
+        pageDarkMode = false;
+    } else {
+        // set to dark mode
+        pageDarkMode = true;
+    }
+}
+
+// implemented
+function saveProject(){  // went to stack overflow to understand how to save files
+    const data = JSON.stringify(instructions); 
+    const blob = new Blob([data], {type: 'application/json'});
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = "project.json";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+// implemented
+function loadProject(e){  // used modzilla to understand how to prompt and upload files
+
+    const file = e.target.files[0];
+    if (file) {
+        reader.readAsText(file);
+    }
+}
+
+// implemented
+function onLoad(e){  // used modzilla
+    const fileContent = e.target.result;
+    try {
+        let uploadedFile = JSON.parse(fileContent);
+        if (Array.isArray(uploadedFile)) {
+            instructions = uploadedFile;
+            console.log("Loaded new project: ");
+            console.log(instructions);
+        } else {
+            alert("Unable to load project.");
+        }
+    } catch (error) {
+        alert("Unable to load project.");
+    }
+};
+
+
+// Functional systems (processing instructions rendering output)
+
+// implemented
 function drawImage(){
     instructions.forEach(inst => {
     console.log(inst);
@@ -56,6 +156,7 @@ function drawImage(){
     });
 }
 
+// incomplete
 function run(inst){
     switch(inst.type){
         case("clear"):
@@ -92,3 +193,4 @@ function run(inst){
             break;
     }
 }
+
